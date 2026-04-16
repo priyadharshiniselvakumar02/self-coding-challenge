20b06833ce8ac01e8b0d5486a72d61ccacf20eb6
priyadharshiniselvakumar02 <priyapriya022005@gmail.com>
2026-04-16 21:49:23 +0530

    Add files via upload

---
+++ substrings1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class substrings1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<=n;j++){
+                System.out.println(s.substring(i,j));
+
+            }
+        }
+    }
+}
